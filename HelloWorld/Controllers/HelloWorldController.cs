d5b2b0a64cdb452ac7fdb9c2432add0eb9a27e79
YourBoB <zhaojunbao123@163.com>
2016-11-22 23:02:41 +1300

    this is my first .net MVC project

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name,int numTimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+        public string hello() {
+            return "this is the hello action method...";
+        }
+    }
+}
