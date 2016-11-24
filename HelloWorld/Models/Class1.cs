7db1fe4c20954b47d23fe76f7bffbb6fc897e305
YourBoB <zhaojunbao123@163.com>
2016-11-25 10:27:09 +1300

    first .net project

---
+++ HelloWorld/Models/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace HelloWorld.Models
+{
+    public class Movie
+    {
+        public int ID { set; get; }
+        public string Title { set; get; }
+        public DateTime ReleaseDate { set; get; }
+        public string Genre { set; get; }
+        public decimal Price { set; get; }
+    }
+    public class MovieDBContext : DbContext
+    {
+    public DbSet<Movie> Movies { set; get; }
+    }
+}
